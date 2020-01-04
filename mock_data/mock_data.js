8ce0e8ea1c4a3baf2d61685679723dc810086ace
Wasuthun Wanaphongthipakorn <johnpaul@Wasuthuns-MacBook-Pro.local>
2020-01-05 01:20:43 +0700

    Create new mock data & add get by type

--- mock_data/mock_data.js
+++ mock_data/mock_data.js
@@ -1,15 +1,42 @@
 require('dotenv').config({ path: '../.env' })
 const mongoose = require("mongoose")
 const News = require('../models/news')
-var News1 = new News(
+const arr = [
+    { 
+        title:'jamie',
+        description:'potential',
+        type:'club'
+    },
+    { 
+        title:'jamie',
+        description:'potential',
+        type:'club'
+    },
     { 
         title:'varit',
         description:'assava',
-        type:'club'
+        type:'general'
+    },
+    { 
+        title:'varit',
+        description:'assava',
+        type:'general'
     }
-)
-News1.save(function (err, news) {
-    if (err) return console.error(err);
-    console.log(news.title + " saved to collection.")
+]
+News.insertMany(arr, function (err, news) {
+    if (err) return console.error(err)
+    console.log(" saved to collection.")
 })
+// var News1 = new News(
+//     { 
+//         title:'varit',
+//         description:'assava',
+//         type:'club'
+//     }
+// )
+
+// News1.save(function (err, news) {
+//     if (err) return console.error(err);
+//     console.log(news.title + " saved to collection.")
+// })
 
--- routes/news.js
+++ routes/news.js
@@ -10,10 +10,29 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: err.message })
     }
 })
+
 router.post('/', async (req,res) => {
     try {
         const news = new News(req.body)
         news.save()
+        res.json(news)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+router.get('/club', async (req, res) => {
+    try {
+        const news = await News.find({ type: 'club' })
+        res.json(news)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+router.get('/general', async (req, res) => {
+    try {
+        const news = await News.find({ type: 'general' })
         res.json(news)
     } catch (err) {
         res.status(500).json({ message: err.message })
