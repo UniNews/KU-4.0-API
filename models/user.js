ad155ef5f725d186f6c9abb44f07726ee634cd14
kakmond <wongsathorn.pa@ku.th>
2020-01-07 22:31:55 +0700

    added a model for User collection

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("../configs/database")
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    displayName: {
+        type: String,
+        required: true
+    },
+})
+
+userSchema.pre('save', function (next) {
+    var user = this
+    if (!user.isModified('password')) return next()
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err)
+            return next(err)
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err)
+            user.password = hash
+            next()
+        })
+    })
+})
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err)
+            return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+module.exports = mongoose.model('User', userSchema)
