1e7bf7ead4530b1f0a4d32c74e1f87dab58d86a9
wasuthun <pond_zen@hotmail.com>
2020-02-25 14:36:58 +0700

    modify auth and data in base

--- mock_data/comments.js
+++ mock_data/comments.js
@@ -4,18 +4,18 @@ const mongoose = require('mongoose')
 const arr = [
     {
         text: "ที่สุดเลยเว่ยแก",
-        user: mongoose.Types.ObjectId('5e32a25f3e65ba7204144325'),
+        user: mongoose.Types.ObjectId('5e54c91c2c9d7dfb05079247'),
         like: [
-            mongoose.Types.ObjectId('5e32a25f3e65ba7204144325'),
-            mongoose.Types.ObjectId('5e32a25f3e65ba7204144324')
+            mongoose.Types.ObjectId('5e54c91c2c9d7dfb05079245'),
+            mongoose.Types.ObjectId('5e54c91c2c9d7dfb05079247')
         ]
     },
     {
         text: "น่าสนใจ",
-        user: mongoose.Types.ObjectId('5e32a25f3e65ba7204144323'),
+        user: mongoose.Types.ObjectId('5e54c91c2c9d7dfb05079247'),
         like: [
-            mongoose.Types.ObjectId('5e32a25f3e65ba7204144324'),
-            mongoose.Types.ObjectId('5e32a25f3e65ba7204144323')
+            mongoose.Types.ObjectId('5e54c91c2c9d7dfb05079248'),
+            mongoose.Types.ObjectId('5e54c91c2c9d7dfb05079249')
         ]
     }
 ]
--- mock_data/users.js
+++ mock_data/users.js
@@ -2,12 +2,11 @@ const User = require('../models/user')
 
 const arr = [
     {
-        username: 'kuphoto',
         password: '5910545833',
         displayName: 'KU Photo CLUB',
         accessType: 'club',
-        avatarURl: 'https://scontent-sin2-1.xx.fbcdn.net/v/t1.0-9/39700613_2009471645785153_2911180338476613632_o.jpg?_nc_cat=111&_nc_ohc=WD9vW0dJfLQAX-emlap&_nc_ht=scontent-sin2-1.xx&oh=637e5e10ad6cc72ea50796e1fb3e3912&oe=5EDBCA78',
-        email: 'test@ku.ac.th',
+        avatarURL: 'https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/260348_165282603537409_3541000_n.jpg?_nc_cat=105&_nc_ohc=mspb4GiagJoAX8yWtDv&_nc_ht=scontent.fbkk22-2.fna&oh=5426f69d27aea9252b12f6f3d0d86472&oe=5EF9B923',
+        email: 'kuphoto@ku.ac.th',
         mobilePhone: '0936784333',
         category: 'ประเทืองปัญญา',
         description: 'ถ่ายรูป',
@@ -15,15 +14,16 @@ const arr = [
         address: 'kasetsart university',
         fbPage: 'https://www.facebook.com/kuphotoclub/posts/2948291431903165',
         follower: [],
+        likeNews:[],
+        likeCommunity:[],
         active: true
     },
     {
-        username: 'kuarsa',
         password: '12345',
         displayName: 'ชมรมค่ายอาสาพัฒนา มหาวิทยาลัยเกษตรศาสตร์',
         accessType: 'club',
-        avatarURl: 'https://scontent-sin2-1.xx.fbcdn.net/v/t1.0-9/52487965_2114158535340833_6446977534587305984_n.jpg?_nc_cat=107&_nc_ohc=NftJ9AOLqvgAX9wTsi7&_nc_ht=scontent-sin2-1.xx&oh=4b3fd45f966a89c6526be8a7abbe4ecd&oe=5EDA2211',
-        email: 'test@ku.ac.th',
+        avatarURL: 'https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/14650254_1138459696244060_4004163000038598621_n.jpg?_nc_cat=107&_nc_ohc=i3qpVduwFDgAX9MZXl_&_nc_ht=scontent.fbkk22-2.fna&oh=5202616cffd8fd265e4c7a71cef53a5e&oe=5F033CD1',
+        email: 'kuarsa@ku.ac.th',
         mobilePhone: '0936784333',
         category: 'พัฒนาตัวเอง',
         description: 'ช่วยเหลือชุมชน',
@@ -31,23 +31,23 @@ const arr = [
         address: 'kasetsart university',
         fbPage: 'https://www.facebook.com/kuvolunteergroup/posts/2510919652331384',
         follower: [],
+        likeNews:[],
+        likeCommunity:[],
         active: true
     },
     {
-        username: 'kuapp',
         password: '1134',
         displayName: 'ประชาสัมพันธ์ มหาวิทยาลัยเกษตรศาสตร์',
         accessType: 'admin',
-        avatarURl: 'https://scontent-sin2-1.xx.fbcdn.net/v/t31.0-8/178263_465729813479359_751914567_o.jpg?_nc_cat=102&_nc_ohc=nmeXmwEglM0AX9ipmdt&_nc_ht=scontent-sin2-1.xx&oh=483fa9033e60715e0704f98d5595f25f&oe=5EDAABD8',
-        email: 'test@ku.ac.th'
+        avatarURL: 'https://upload.wikimedia.org/wikipedia/commons/9/97/KU_Logo.png',
+        email: 'kuapp@ku.ac.th'
     },
     {
-        username: 'kumaku',
         password: '1123',
         displayName: 'Kuma Shabu',
         accessType: 'store',
-        avatarURl: 'https://scontent-sin2-1.xx.fbcdn.net/v/t1.0-9/22852956_928949130588509_1852747991864105926_n.jpg?_nc_cat=106&_nc_ohc=ITUyP0zNi0UAX-UZAXG&_nc_ht=scontent-sin2-1.xx&oh=09f93774b423f65323b2396324af82ae&oe=5ED878D2',
-        email: 'test@ku.ac.th',
+        avatarURL: 'https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/15027487_715730821910342_9089226803614118721_n.jpg?_nc_cat=105&_nc_ohc=OxtWbIhGyz8AX8dEmaX&_nc_ht=scontent.fbkk22-2.fna&oh=0ac5b0182b2eba39d555878cfdc66b1c&oe=5EB74E90',
+        email: 'kumaku@ku.ac.th',
         mobilePhone: '0936784333',
         category: 'ชาบู',
         description: 'บุฟเฟ่ 299',
@@ -55,37 +55,42 @@ const arr = [
         address: 'kasetsart university',
         fbPage: 'https://www.facebook.com/KumaShabu/posts/1496956427121107',
         follower: [],
+        likeNews:[],
+        likeCommunity:[],
         active: true
     },
     {
-        username: 'jamie',
         password: '112',
         displayName: 'jamie',
-        avatarURl: 'https://scontent.fbkk22-1.fna.fbcdn.net/v/t31.0-8/913957_540038519399895_1074698814_o.jpg?_nc_cat=101&_nc_eui2=AeF7qiZikqeeuE0hlZteTMPlps_bwxitbHk6eaaVWbosVhSLU69t0TFddrZBSzvYOPwRQbq_lc5CyX_Slrdbq2rvCS_4fbFAVWkRmd36od20ow&_nc_ohc=VdDFKI0OUAAAX8HNkZV&_nc_ht=scontent.fbkk22-1.fna&oh=99db65f5322ef348e0e780805c52f621&oe=5E9969BD',
-        email: 'test@ku.ac.th',
+        avatarURL: 'https://scontent.fbkk2-5.fna.fbcdn.net/v/t1.0-1/c0.0.820.820a/66686017_1125283600997160_4542151837934944256_n.jpg?_nc_cat=110&_nc_oc=AQmNpBihC6kLplVjqVJTIrCFMMm59mZi4qLk9VaYlJEaVjaWaGnRNVpvjGEXBl3tINA&_nc_ht=scontent.fbkk2-5.fna&oh=326e11cf10014a155dd953be098951f0&oe=5E90FC5F',
+        email: 'jamie@ku.ac.th',
         mobilePhone: '0946878555',
         active: true,
         description: 'ซอฟต์แวร์และความรู้',
-        following: []
+        following: [],
+        likeNews:[],
+        likeCommunity:[]
     },
     {
-        username: 'mond',
         password: '123',
         displayName: 'mond',
-        email: 'test@ku.ac.th',
+        email: 'mond@ku.ac.th',
         mobilePhone: '0946878555',
         active: true,
         description: 'ซอฟต์แวร์และความรู้',
+        likeNews:[],
+        likeCommunity:[],
         following: []
     },
     {
-        username: 'paul',
         password: '113',
         displayName: 'paul',
-        email: 'test@ku.ac.th',
+        email: 'paul@ku.ac.th',
         mobilePhone: '0946878555',
         active: true,
         description: 'ซอฟต์แวร์และความรู้',
+        likeNews:[],
+        likeCommunity:[],
         following: []
     }
 ]
