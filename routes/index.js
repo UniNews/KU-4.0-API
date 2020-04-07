f61bac2438653bea3361ac1c6eca49865d070f20
puow-mond <wongsathorn.pa@ku.th>
2020-04-07 17:43:09 +0700

    not require auth on /images route

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use('/profile', auth.required, require('./profile'))
 router.use('/notifications', auth.required, require('./notifications'))
 router.use('/users', auth.required, require('./users'))
 router.use('/articles', auth.required, require('./articles'))
-router.use('/images', auth.required, require('./images'))
+router.use('/images', require('./images'))
 
 router.use(function (err, req, res, next) {
     if (err instanceof mongoose.Error.CastError)
