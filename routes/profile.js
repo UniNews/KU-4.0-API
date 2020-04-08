1ba3a7218049aa627b660d87408f0bdd8382805f
puow-mond <wongsathorn.pa@ku.th>
2020-04-08 17:20:43 +0700

    added ban and edit user profile (admin only)

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
+const { check, validationResult } = require('express-validator')
 
 router.use(async function (req, res, next) {
     const user = await User.findById(req.payload.id)
@@ -52,46 +53,61 @@ router.get('/followers', async function (req, res) {
     }
 })
 
-router.put('/', async function (req, res, next) {
-    try {
-        const user = req.user
-        // only update fields that were actually passed
-        const displayName = req.body.displayName
-        const avatarURL = req.body.avatarURL
-        const bio = req.body.bio
-        if (typeof displayName !== 'undefined')
-            user.displayName = displayName
-        if (typeof avatarURL !== 'undefined')
-            user.avatarURL = avatarURL
-        if (typeof bio !== 'undefined')
-            user.bio = bio
-        // if the role is store or admin, update additional fields...
-        if (user.role != 'user') {
-            const email = req.body.email
-            const firstName = req.body.firstName
-            const lastName = req.body.lastName
-            const mobilePhone = req.body.mobilePhone
-            const tags = req.body.tags
-            const contacts = req.body.contacts
-            if (typeof email !== 'undefined')
-                user.email = email
-            if (typeof firstName !== 'undefined')
-                user.firstName = firstName
-            if (typeof lastName !== 'undefined')
-                user.lastName = lastName
-            if (typeof mobilePhone !== 'undefined')
-                user.mobilePhone = mobilePhone
-            if (typeof tags !== 'undefined')
-                user.tags = tags
-            if (typeof contacts !== 'undefined')
-                user.contacts = contacts
+router.put('/',
+    [
+        check('displayName').optional().isLength({ min: 3, max: 20 }).withMessage('displayName must be between 3 and 20 chars long.'),
+        check('avatarURL').optional().isURL().withMessage('avatarURL must be an URL.'),
+        check('bio').optional(),
+        check('email').optional().isEmail().withMessage('invalid email.'),
+        check('firstName').optional(),
+        check('lastName').optional(),
+        check('mobilePhone').optional().isNumeric().withMessage('mobilePhone must be number only.'),
+        check('tags').optional().isArray().withMessage('tags must be an array.'),
+        check('contacts').optional(),
+    ],
+    async function (req, res, next) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty())
+                return res.status(422).json({ errors: errors.array().map(error => error) })
+            const user = req.user
+            // only update fields that were actually passed
+            const displayName = req.body.displayName
+            const avatarURL = req.body.avatarURL
+            const bio = req.body.bio
+            if (typeof displayName !== 'undefined')
+                user.displayName = displayName
+            if (typeof avatarURL !== 'undefined')
+                user.avatarURL = avatarURL
+            if (typeof bio !== 'undefined')
+                user.bio = bio
+            // if the role is store or admin, update additional fields...
+            if (user.role != 'user') {
+                const email = req.body.email
+                const firstName = req.body.firstName
+                const lastName = req.body.lastName
+                const mobilePhone = req.body.mobilePhone
+                const tags = req.body.tags
+                const contacts = req.body.contacts
+                if (typeof email !== 'undefined')
+                    user.email = email
+                if (typeof firstName !== 'undefined')
+                    user.firstName = firstName
+                if (typeof lastName !== 'undefined')
+                    user.lastName = lastName
+                if (typeof mobilePhone !== 'undefined')
+                    user.mobilePhone = mobilePhone
+                if (typeof tags !== 'undefined')
+                    user.tags = tags
+                if (typeof contacts !== 'undefined')
+                    user.contacts = contacts
+            }
+            await user.save()
+            return res.json(user)
         }
-        await user.save()
-        return res.json(user)
-    }
-    catch (err) {
-        return next(err)
-    }
-})
+        catch (err) {
+            return next(err)
+        }
+    })
 
 module.exports = router 
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 const filter = require('../middlewares/filter')
+const { check, validationResult } = require('express-validator')
 
 // preload user objects on routes with ':id'
 router.param('user', async function (req, res, next, id) {
@@ -151,5 +152,109 @@ router.delete('/:user/follow', async function (req, res, next) {
         return next(err)
     }
 })
+
+/* for admin only */
+router.post('/:user/ban', async function (req, res, next) {
+    try {
+        const targetUser = req.user
+        const myUser = await User.findById(req.payload.id)
+        if (!myUser)
+            return res.sendStatus(401)
+        if (myUser.role === 'admin') {
+            targetUser.active = false
+            await targetUser.save()
+            return res.sendStatus(204)
+        }
+        else
+            return res.sendStatus(403)
+    }
+    catch (err) {
+        return next(err)
+    }
+})
+
+router.delete('/:user/ban', async function (req, res, next) {
+    try {
+        const targetUser = req.user
+        const myUser = await User.findById(req.payload.id)
+        if (!myUser)
+            return res.sendStatus(401)
+        if (myUser.role === 'admin') {
+            targetUser.active = true
+            await targetUser.save()
+            return res.sendStatus(204)
+        }
+        else
+            return res.sendStatus(403)
+    }
+    catch (err) {
+        return next(err)
+    }
+})
+
+
+router.put('/:user',
+    [
+        check('displayName').optional().isLength({ min: 3, max: 20 }).withMessage('displayName must be between 3 and 20 chars long.'),
+        check('avatarURL').optional().isURL().withMessage('avatarURL must be an URL.'),
+        check('bio').optional(),
+        check('email').optional().isEmail().withMessage('invalid email.'),
+        check('firstName').optional(),
+        check('lastName').optional(),
+        check('mobilePhone').optional().isNumeric().withMessage('mobilePhone must be number only.'),
+        check('tags').optional().isArray().withMessage('tags must be an array.'),
+        check('contacts').optional(),
+    ],
+    async function (req, res, next) {
+        try {
+            const myUser = await User.findById(req.payload.id)
+            if (!myUser)
+                return res.sendStatus(401)
+            if (myUser.role === 'admin') {
+                const errors = validationResult(req)
+                if (!errors.isEmpty())
+                    return res.status(422).json({ errors: errors.array().map(error => error) })
+                const user = req.user
+                // only update fields that were actually passed
+                const displayName = req.body.displayName
+                const avatarURL = req.body.avatarURL
+                const bio = req.body.bio
+                if (typeof displayName !== 'undefined')
+                    user.displayName = displayName
+                if (typeof avatarURL !== 'undefined')
+                    user.avatarURL = avatarURL
+                if (typeof bio !== 'undefined')
+                    user.bio = bio
+                // if the role is store or admin, update additional fields...
+                if (user.role != 'user') {
+                    const email = req.body.email
+                    const firstName = req.body.firstName
+                    const lastName = req.body.lastName
+                    const mobilePhone = req.body.mobilePhone
+                    const tags = req.body.tags
+                    const contacts = req.body.contacts
+                    if (typeof email !== 'undefined')
+                        user.email = email
+                    if (typeof firstName !== 'undefined')
+                        user.firstName = firstName
+                    if (typeof lastName !== 'undefined')
+                        user.lastName = lastName
+                    if (typeof mobilePhone !== 'undefined')
+                        user.mobilePhone = mobilePhone
+                    if (typeof tags !== 'undefined')
+                        user.tags = tags
+                    if (typeof contacts !== 'undefined')
+                        user.contacts = contacts
+                }
+                await user.save()
+                return res.json(user)
+            }
+            else
+                return res.sendStatus(403)
+        }
+        catch (err) {
+            return next(err)
+        }
+    })
 
 module.exports = router 
