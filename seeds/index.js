4a324f763e5c88da9d7f5d508afd5d671a119d83
Jamie Sathira <nong.jamie.jamie@gmail.com>
2020-04-18 20:21:30 +0700

    imported every previous edited seeds into the seeds/index.js file

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,27 @@
 const kuacoustic = require('./store/kuacoustic')
+const kuchorus = require('./store/kuchorus')
+const kudanceclub = require('./store/kudanceclub')
+const kumashabu = require('./store/kumashabu')
+const kuphotoclub = require('./store/kuphotoclub')
+const kupotentialclub = require('./store/kupotentialclub')
+const kuvolunteergroup = require('./store/kuvolunteergroup')
+const nskkasetsart = require('./store/nskkasetsart')
+const samsteak = require('./store/samsteak')
+const speakerku = require('./store/speakerku')
+const yujinshabu = require('./store/yujinshabu')
 
 const insertMany = async () => {
-    await kuacoustic()
+    await kuacoustic(),
+    await kuchorus(),
+    await kudanceclub(),
+    await kumashabu(),
+    await kuphotoclub(),
+    await kupotentialclub(),
+    await kuvolunteergroup(),
+    await nskkasetsart(),
+    await samsteak(),
+    await speakerku(),
+    await yujinshabu()
 }
 
 module.exports = insertMany
